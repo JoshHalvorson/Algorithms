96c0435831f7c8586857d2c690afaa7fdbff24d7
JoshHalvorson <joshhalvorson16@gmail.com>
2019-09-11 11:16:36 -0700

    Add eating cookies solution

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,8 +5,24 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
+
+cache = {}
+
 def eating_cookies(n, cache=None):
-  pass
+  if cache != None:
+    if n in cache:
+      return cache[n]
+  if n == 0:
+    cache[n] = 1
+    return cache[n]
+  if n < 1:
+    cache[n] = 0
+    return cache[n]
+  result = eating_cookies(n - 1, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 3, cache)
+  if cache != None:
+    cache[n] = result
+  return result
+  
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
