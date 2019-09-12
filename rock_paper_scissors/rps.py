fb7deaa86ba79b2e400eee0cf7eb7f68ceee78f3
JoshHalvorson <joshhalvorson16@gmail.com>
2019-09-12 13:42:32 -0700

    Add rps solution

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,8 +3,20 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  plays = ['rock', 'paper', 'scissors']
+  result = []
+  buildArr(plays, result, [], n)
+  return result
 
+def buildArr(plays, result, arr, n):
+  # base case
+  # if no more plays to be added to arr, append the arr to result
+  if n == 0:
+    result.append(arr)
+    return
+  # loop through the plays, recursivley call builArr to build up the arr with n plays
+  for play in plays:
+    buildArr(plays, result, arr + [play], n - 1)
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
